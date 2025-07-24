80bfac095b5586feadbf5ccc3bd9380c4b745ad4
renyuneyun (Rui Zhao) <renyuneyun@gmail.com>
2025-07-24 01:39:52 +0100

    Migrate to package:web instead of dart:http
    - For better interoperability, and future-proof
    - Support wasm build

--- lib/src/openid/openid_client_browser.dart
+++ lib/src/openid/openid_client_browser.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: avoid_web_libraries_in_flutter
 
 import 'openid_client.dart';
-import 'dart:html' hide Credential, Client;
+import 'package:web/web.dart' show window;
 import 'dart:async';
 import 'dart:convert';
 export 'openid_client.dart';
@@ -19,7 +19,7 @@ class Authenticator {
     Iterable<String> scopes = const [],
     popToken = '',
   }) : this._(Flow.authorizationCodeWithPKCE(client,
-            state: window.localStorage['openid_client:state'])
+            state: window.localStorage.getItem('openid_client:state'))
           ..scopes.addAll(scopes)
           ..redirectUri = Uri.parse(window.location.href.contains('#/')
                   ? window.location.href.replaceAll('#/', 'callback.html')
@@ -29,7 +29,7 @@ class Authenticator {
 
   void authorize() {
     _forgetCredentials();
-    window.localStorage['openid_client:state'] = flow.state;
+    window.localStorage.setItem('openid_client:state', flow.state);
     window.location.href = flow.authenticationUri.toString();
   }
 
@@ -49,21 +49,21 @@ class Authenticator {
   }
 
   void _forgetCredentials() {
-    window.localStorage.remove('openid_client:state');
-    window.localStorage.remove('openid_client:auth');
+    window.localStorage.removeItem('openid_client:state');
+    window.localStorage.removeItem('openid_client:auth');
   }
 
   static Future<Credential?> _credentialFromUri(Flow flow) async {
     Map? q;
-    if (window.localStorage.containsKey('openid_client:auth')) {
-      q = json.decode(window.localStorage['openid_client:auth']!);
+    if (window.localStorage.getItem('openid_client:auth') != null) {
+      q = json.decode(window.localStorage.getItem('openid_client:auth')!);
     } else {
       var uri = Uri(query: Uri.parse(window.location.href).fragment);
       q = uri.queryParameters;
       if (q.containsKey('access_token') ||
           q.containsKey('code') ||
           q.containsKey('id_token')) {
-        window.localStorage['openid_client:auth'] = json.encode(q);
+        window.localStorage.setItem('openid_client:auth', json.encode(q));
         window.location.href =
             Uri.parse(window.location.href).removeFragment().toString();
       }
